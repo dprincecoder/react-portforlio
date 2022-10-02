e32f8aa98d71987c5fbc95145721da233df2e341
dprincecoder <dprincecoder@gmail.com>
2022-10-02 01:10:07 +0100

    updated the about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+
+import './about.css'
 
 const About = () => {
   return (
@@ -9,12 +14,35 @@ const About = () => {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-img">
-            <img src="assets" alt="" />
-              
+            <img src="assets/pic-2.jpg" alt="me" />
           </div>
         </div>
 
-        <div className="about__content"></div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>2+ Years Working</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>50+ Worldwide</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>100+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, perspiciatis molestiae porro pariatur quod cum autem excepturi vitae sapiente dolorem labore ea, quia reprehenderit eveniet tempora dignissimos nisi nobis. Libero.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
       </div>
     </section>
   );
