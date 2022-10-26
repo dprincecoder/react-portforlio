8e60016b3ec416d7fe2a48b6922b8517b5038f34
Dprincecoder <78590038+dprincecoder@users.noreply.github.com>
2022-10-26 10:50:56 +0100

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
   const sendMail = (e) => {
     e.preventDefault();
     emailjs
-      .sendForm(`service_id`, `template_id`, formHandler.current, `user_id`)
+      .sendForm(`seevice_yzjd2hq`, `template_2i7xmhp`, formHandler.current, `user_uwncvCvZxogLFugyV`)
       .then(
         (result) => {
           console.log(result.text);
